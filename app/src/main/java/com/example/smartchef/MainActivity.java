d27a4276636b3d0c9a0f812c9b567bb6fe2b5b8f
PANCnnss <pedronuca3@gmail.com>
2020-07-06 14:02:24 -0500

    Creación inicial

--- app/src/main/java/com/example/smartchef/MainActivity.java
+++ app/src/main/java/com/example/smartchef/MainActivity.java
@@ -1,10 +1,44 @@
 package com.example.smartchef;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+/*
+Página de Login (inicio)
+ */
 
 public class MainActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        mAuth = FirebaseAuth.getInstance();
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        try {
+            if(mAuth==null) mAuth = FirebaseAuth.getInstance();
+            FirebaseUser cUser = mAuth.getCurrentUser();
+        }catch (Exception e){
+            Log.e("ERROR","Something");
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
